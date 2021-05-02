b950d716c5ee0e1f053856de50ed834d400d060b
Messias Junior <messias.junior@tui.co.uk>
2021-05-02 23:39:59 +0100

    first commit

---
+++ app/src/main/java/com/messias/designthinkingmethods/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.messias.designthinkingmethods
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
